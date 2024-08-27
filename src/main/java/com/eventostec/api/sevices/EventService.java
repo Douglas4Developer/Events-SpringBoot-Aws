3f335ecee5a6c1b371f1ff5b24c586291aabb7ac
douglas <douglas8_ferreira@hotmail.com>
2024-08-27 09:16:49 -0300

    update project

--- src/main/java/com/eventostec/api/sevices/EventService.java
+++ src/main/java/com/eventostec/api/sevices/EventService.java
@@ -3,17 +3,21 @@ package com.eventostec.api.sevices;
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -28,6 +32,9 @@ public class EventService {
 
     @Autowired
     private EventRepository repository;
+
+    @Autowired
+    private AddressService addressService;
 
     public Event createEvent(EventRequestDTO data){
         String imgUrl = null;
@@ -46,9 +53,46 @@ public class EventService {
 
         repository.save(newEvent);
 
+        if(!data.remote()){
+            this.addressService.createAddress(data, newEvent);
+        }
+
         return newEvent;
         }
 
+    public List<EventResponseDTO> getUpcomingEvents(int page, int size){
+      Pageable pageable =  PageRequest.of(page, size);
+      Page<Event> eventsPage = repository.findUpcomingEvents(new Date(), pageable);
+      return eventsPage.map(event -> new EventResponseDTO(event.getId(),event.getTitle(),event.getDescription(),
+              event.getDate(),"", "", event.getRemote(), event.getEventUrl(), event.getImgUrl()))
+              .stream().toList();
+
+
+    }
+
+
+    public List<EventResponseDTO> getFilteredEvents(int page, int size, String city, String uf, Date startDate, Date endDate){
+        city = (city != null) ? city : "";
+        uf = (uf != null) ? uf : "";
+        startDate = (startDate != null) ? startDate : new Date(0);
+        endDate = (endDate != null) ? endDate : new Date();
+
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Event> eventsPage = this.repository.findFilteredEvents(city, uf, startDate, endDate, pageable);
+        return eventsPage.map(event -> new EventResponseDTO(
+                        event.getId(),
+                        event.getTitle(),
+                        event.getDescription(),
+                        event.getDate(),
+                        event.getCity() != null ? event.getCity() : "",
+                        event.getUf() != null ? event.getUf() : "",
+                        event.getRemote(),
+                        event.getEventUrl(),
+                        event.getImgUrl())
+                )
+                .stream().toList();
+    }
     public String uploadImg(MultipartFile multipartFile){
 
         // Fazendo upload na AMAZON S3 (AWS)
